0aa410914b213295d3ae887ff4544448b9ac1ec5
Apolinapolis <larra12@ya.ru>
2025-01-28 13:38:41 +0300

    6 hw

--- qa_guru.py
+++ qa_guru.py
@@ -7,8 +7,13 @@ def test_dark_theme_by_time():
     """
     current_time = time(hour=23)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
-
     is_dark_theme = None
+
+    if time(hour=6) < current_time < time(hour=22):
+        is_dark_theme = False
+    else:
+        is_dark_theme = True
+
     assert is_dark_theme is True
 
 
@@ -24,8 +29,17 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = True
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
-
     is_dark_theme = None
+
+    if dark_theme_enabled_by_user:
+        is_dark_theme = True
+    elif not dark_theme_enabled_by_user:
+        is_dark_theme = False
+    elif time(hour=6) < current_time < time(hour=22):
+        is_dark_theme = False
+    else:
+        is_dark_theme = True
+
     assert is_dark_theme is True
 
 
@@ -42,11 +56,26 @@ def test_find_suitable_user():
     ]
 
     # TODO найдите пользователя с именем "Olga"
-    suitable_users = None
+    suitable_users = 0
+
+    for user in users:
+        if user['name'] == "Olga":
+            suitable_users = user
+            break
+        else:
+            continue
+
     assert suitable_users == {"name": "Olga", "age": 45}
 
     # TODO найдите всех пользователей младше 20 лет
-    suitable_users = None
+    suitable_users = []
+
+    for user in users:
+        if user['age'] < 20:
+            suitable_users.append(user)
+        else:
+            continue
+
     assert suitable_users == [
         {"name": "Stanislav", "age": 15},
         {"name": "Maria", "age": 18},
@@ -71,15 +100,19 @@ def test_readable_function():
 
 
 def open_browser(browser_name):
-    actual_result = None
+    n = f'[{browser_name}]'
+    actual_result = f'{open_browser.__name__} {n}'.replace('_'," ").title()
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    n = f'[{page_url}]'
+    actual_result = f'{go_to_companyname_homepage.__name__}'.replace('_'," ").title() + f' {n}'
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    name = find_registration_button_on_login_page.__name__.replace("_"," ").title()
+    site = f'[{page_url}, {button_text}]'
+    actual_result = f"{name} {site}"
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
