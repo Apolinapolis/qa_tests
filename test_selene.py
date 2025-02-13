c4837a2003e0de4577810661933100edf1b9469a
Apolinapolis <larra12@ya.ru>
2025-02-13 18:19:45 +0300

    Allure Reports

--- test_selene.py
+++ test_selene.py
@@ -1,5 +1,6 @@
 from time import sleep
 
+import allure
 from selene import browser
 from selene.support import by
 from selene.support.conditions import be
@@ -7,15 +8,20 @@ from selene.support.shared.jquery_style import s
 
 
 def test_github():
-    browser.open('https://github.com/')
-    s(".mr-2.color-fg-muted").click()
-    s(".FormControl-input").send_keys('eroshenkoam/allure-example')
-    s(".FormControl-input").submit()
 
-    s(by.link_text('eroshenkoam/allure-example')).click()
+    with allure.step('open main page'):
+        browser.open('https://github.com/')
 
-    s("#issues-repo-tab-count").click()
+    with allure.step('searching repo'):
+        s(".mr-2.color-fg-muted").click()
+        s(".FormControl-input").send_keys('eroshenkoam/allure-example')
+        s(".FormControl-input").submit()
 
-    s(by.text('95')).should(be.visible)
+    with allure.step('click on repo_name'):
+        s(by.link_text('eroshenkoam/allure-example')).click()
 
-    sleep(3)
+    with allure.step('click on issues icon'):
+        s("#issues-repo-tab-count").click()
+
+    with allure.step('check - find text'):
+        s(by.text('Тестируем тест')).should(be.visible)
